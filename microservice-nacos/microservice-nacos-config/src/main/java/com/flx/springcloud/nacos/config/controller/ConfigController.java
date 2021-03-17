1575f71c6115f564d23f463fbc947cfb0d091cee
fenglixiong <fenglixiong@flashhold.com>
2021-03-17 11:19:38 +0800

    nacos服务注册与发现sdk

--- microservice-nacos/microservice-nacos-config/src/main/java/com/flx/springcloud/nacos/config/controller/ConfigController.java
+++ microservice-nacos/microservice-nacos-config/src/main/java/com/flx/springcloud/nacos/config/controller/ConfigController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @Author: Fenglixiong
  * @Date: 2020/12/18 15:54
- * @Description:
+ * @Description: 注意：配置也可以写在service中，但是必须要注意分隔符是.而不能用_
  */
 @Slf4j
 @RestController
